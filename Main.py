db4f1c2e520a82af4bbdd3d4a3a1887eb447e550
pwarawit <p.warawit@gmail.com>
2014-11-19 01:01:08 +0700

    add more feature

--- Main.py
+++ Main.py
@@ -4,27 +4,39 @@ Created on Nov 18, 2014
 @author: InfoMobius
 '''
 
+import sys
 from PyQt4 import QtGui
 
-class HelloPython(QtGui.QWidget):
+class MainWindow(QtGui.QWidget):
+    
     def __init__(self, parent=None):
-        super(HelloPython, self).__init__(parent)
-        helloLabel = QtGui.QLabel("Say Hello To PyQT!")       
-        helloLineEdit = QtGui.QLineEdit()
- 
-        mainLayout = QtGui.QGridLayout()
-        mainLayout.addWidget(helloLabel, 0, 0)
-        mainLayout.addWidget(helloLineEdit, 0, 1)
- 
-        self.setLayout(mainLayout)
+        super(MainWindow, self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        # Initialize main window
+        self.resize(250,150)
+        self.center()
         self.setWindowTitle("MDC Data Preparation")
+        
+        # Initialize button
+        btn = QtGui.QPushButton('Run', self)
+        btn.resize(btn.sizeHint())
+        btn.move(50,50)
+        
+        self.show()
+        
+    def center(self):
+        # Put the MainWindow into the center of screen
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    myMainWindow = MainWindow()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    import sys
- 
-    app = QtGui.QApplication(sys.argv)
- 
-    helloPythonWidget = HelloPython()
-    helloPythonWidget.show()
- 
-    sys.exit(app.exec_())
+    main()
